ceb10d97a6699cb63bb67a18fb13fb4ad8b7e662
Ivan Gainullin <gainullin.ii@phystech.edu>
2021-10-15 00:36:19 +0300

    Added function ChooseTerminal

--- FifoAlltogether.c
+++ FifoAlltogether.c
@@ -5,11 +5,41 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 
+void ChooseTerminal (char* a, char* b, char* buffer, char* buffer_1, int fd1, int fd2) {
+	pid_t result = fork();
+	
+	if (result < 0) {
+		printf("Can't fork children\n");
+		exit(-1);
+	}
+		
+	if (result > 0) {
+		fd1 = open(a, O_WRONLY);
+		
+		while (1) {
+			fgets(buffer, 100, stdin);
+			write(fd1, buffer, 100);
+		}
+	} else {
+		fd2 = open(b, O_RDONLY);
+	
+		while (1) {
+			read(fd2, buffer_1, 100);
+			printf("%s", buffer_1);
+		}
+	}
+}
+
 int main(int argc, char* argv[]) {
 	(void)umask(0);
 	
 	char a[] = "a.fifo";
 	char b[] = "b.fifo";
+	
+	char buffer[100];
+	char buffer_1[100];
+	
+	int fd1, fd2;
 	
 	if (mknod(a, S_IFIFO | 0666, 0) < 0) {
 		printf("Can't create FIFO\n");
@@ -19,50 +49,11 @@ int main(int argc, char* argv[]) {
 		printf("Can't create FIFO\n");
 	}
 	
-	pid_t result = fork();
-	 
-	if (result < 0) {
-		printf("Can't fork children\n");
-		exit(-1);
-	}
-	
-	int fd1, fd2;
-	
 	if (atoi(argv[1]) == 0) {
-		fd1 = open("a.fifo", O_WRONLY);
-		fd2 = open("b.fifo", O_RDONLY);
-		
-		char buffer[100];
-		char buffer_1[100];
-		
-		while (1) {
-			if (result != 0) {
-				fgets(buffer, 100, stdin);
-				write(fd1, buffer, 100);
-			} else {
-				read(fd2, buffer_1, 100);
-				printf("%s", buffer_1);
-			}
-		}
-		
-		return 0;
+		ChooseTerminal(a, b, buffer, buffer_1, fd1, fd2);
 	} else if (atoi(argv[1]) == 1) {
-		fd1 = open("a.fifo", O_RDONLY);
-		fd2 = open("b.fifo", O_WRONLY);
-		
-		char buffer[100];
-		char buffer_1[100];
-		
-		while (1) {
-			if (result != 0) {
-				fgets(buffer, 100, stdin);
-				write(fd2, buffer, 100);
-			} else {
-				read(fd1, buffer_1, 100);
-				printf("%s", buffer_1);
-			}
-		}
-		
-		return 0;
+		ChooseTerminal(b, a, buffer, buffer_1, fd2, fd1);
 	}
+	
+	return 0;
 }
