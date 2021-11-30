b9b746fd85874de77e63b7aa2aa78739d86c4740
Bitman_BT <gainullin.ii@phystech.edu>
2021-11-30 23:27:15 +0300

    Fixed to-do

--- ThreadsFifoMessenger.c
+++ ThreadsFifoMessenger.c
@@ -5,12 +5,17 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
-char a[] = "a.fifo";
-char b[] = "b.fifo";
+struct to_send
+{
+	char a[7];
+	char b[7];
+};
 
-void* ChooseTerminal_0_get (void* dummy) {
-	int fd1 = open(a, O_WRONLY);
+void* ChooseTerminal_get (struct to_send* FD)
+{
+	int fd1 = open(FD->a, O_WRONLY);
 	char buffer[100];
 		
 	while (1) {
@@ -19,8 +24,9 @@ void* ChooseTerminal_0_get (void* dummy) {
 	}
 }
 
-void* ChooseTerminal_0_print (void* dummy) {
-	int fd2 = open(b, O_RDONLY);
+void* ChooseTerminal_print (struct to_send* FD)
+{
+	int fd2 = open(FD->b, O_RDONLY);
 	char buffer_1[100];
 	
 	while (1) {
@@ -29,31 +35,10 @@ void* ChooseTerminal_0_print (void* dummy) {
 	}
 }
 
-void* ChooseTerminal_1_get (void* dummy) {
-	int fd1 = open(b, O_WRONLY);
-	char buffer[100];
-		
-	while (1) {
-		fgets(buffer, 100, stdin);
-		write(fd1, buffer, 100);
-	}
-}
-
-// TODO: у вас ChooseTerminal_0_get отличается от ChooseTerminal_1_get одним параметром. Нужно принять этот параметр в качестве входного аргумента и избавиться от дублирования кода тем самым
-
-void* ChooseTerminal_1_print (void* dummy) {
-	int fd2 = open(a, O_RDONLY);
-	char buffer_1[100];
-	
-	while (1) {
-		read(fd2, buffer_1, 100);
-		printf("%s", buffer_1);
-	}
-}
-
-void ChooseTerminal_0 () {
+void ChooseTerminal(struct to_send* FD)
+{
 	pthread_t thid, thid_new;
-	int result = pthread_create(&thid, (pthread_attr_t *)NULL, ChooseTerminal_0_get, NULL);
+	int result = pthread_create(&thid, (pthread_attr_t *)NULL, ChooseTerminal_get, FD);
 		
 	if(result != 0) {
 		printf ("Error on thread create, return value = %d\n", result);
@@ -62,31 +47,7 @@ void ChooseTerminal_0 () {
 		
 	printf("Thread created, thid = %d\n", thid);
 	
-	int result_new = pthread_create(&thid_new, (pthread_attr_t *)NULL, ChooseTerminal_0_print, NULL);
-		
-	if(result_new != 0) {
-		printf ("Error on thread create, return value = %d\n", result);
-		exit(-1);
-	}
-		
-	printf("Thread created, thid = %d\n", thid_new);
-	
-	pthread_join(thid, (void **)NULL);
-	pthread_join(thid_new, (void **)NULL);
-}
-
-void ChooseTerminal_1 () {
-	pthread_t thid, thid_new;
-	int result = pthread_create(&thid, (pthread_attr_t *)NULL, ChooseTerminal_1_get, NULL);
-		
-	if(result != 0) {
-		printf ("Error on thread create, return value = %d\n", result);
-		exit(-1);
-	}
-		
-	printf("Thread created, thid = %d\n", thid);
-	
-	int result_new = pthread_create(&thid_new, (pthread_attr_t *)NULL, ChooseTerminal_1_print, NULL);
+	int result_new = pthread_create(&thid_new, (pthread_attr_t *)NULL, ChooseTerminal_print, FD);
 		
 	if(result_new != 0) {
 		printf ("Error on thread create, return value = %d\n", result);
@@ -101,19 +62,31 @@ void ChooseTerminal_1 () {
 
 int main(int argc, char* argv[]) {
 	(void)umask(0);
+
+	struct to_send FD;
+
+	strcpy(FD.a, "a.fifo");
+	strcpy(FD.b, "b.fifo");
 	
-	if (mknod(a, S_IFIFO | 0666, 0) < 0) {
+	if (mknod(FD.a, S_IFIFO | 0666, 0) < 0)
+	{
 		printf("Can't create FIFO\n");
 	}
 	
-	if (mknod(b, S_IFIFO | 0666, 0) < 0) {
+	if (mknod(FD.b, S_IFIFO | 0666, 0) < 0)
+	{
 		printf("Can't create FIFO\n");
 	}
-	
-	if (atoi(argv[1]) == 0) {
-		ChooseTerminal_0();
-	} else if (atoi(argv[1]) == 1) {
-		ChooseTerminal_1();
+
+	if (atoi(argv[1]) == 0)
+	{
+		ChooseTerminal(&FD);
+	} 
+	else if (atoi(argv[1]) == 1)
+	{
+		strcpy(FD.a, "b.fifo");
+		strcpy(FD.b, "a.fifo");
+		ChooseTerminal(&FD);
 	}
 	
 	return 0;
